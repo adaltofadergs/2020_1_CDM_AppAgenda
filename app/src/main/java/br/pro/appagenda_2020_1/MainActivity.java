9ae25fd7baf3a0b971c1a12f8a05241ad68844d2
Adalto Selau Sparremberger <adalto.selau@esade.local>
2020-03-06 21:41:17 -0300

    Falta carregar o spinner de cidades

--- app/src/main/java/br/pro/appagenda_2020_1/MainActivity.java
+++ app/src/main/java/br/pro/appagenda_2020_1/MainActivity.java
@@ -1,8 +1,11 @@
 package br.pro.appagenda_2020_1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -31,12 +34,52 @@ public class MainActivity extends AppCompatActivity {
         cbEmail = findViewById(R.id.cbAceitaEmail);
         btnSalvar = findViewById(R.id.btnSalvar);
 
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                salvar();
+            }
+        });
+
     }
 
 
     private void salvar(){
         String texto = "";
-        texto += "Nome: " + etNome.getText().toString() + "\n" ;
+        texto += "Nome: " + etNome.getText().toString() + "\n" +
+             getResources().getString(R.string.lblTelefone) + etTelefone.getText().toString() +"\n"+
+             getResources().getString(R.string.lblCidade) + spCidade.getSelectedItem().toString()+"\n"+
+             getResources().getString(R.string.lblEstado) + spEstado.getSelectedItem().toString()+"\n" +
+             getResources().getString(R.string.lblSexo);
+        if( rbFeminino.isChecked() ){
+            texto += getResources().getString(R.string.lblFeminino);
+        }else if( rbMasculino.isChecked() ){
+            texto += rbMasculino.getText();
+        }else {
+            texto +="(não informado)";
+        }
+        texto += "\n" + getResources().getString(R.string.lblAceitaEmail);
+        if ( cbEmail.isChecked() ){
+            texto += "SIM";
+        }else {
+            texto += "NÃO";
+        }
+
+        texto += "\n\nDeseja salvar?";
+
+        AlertDialog.Builder alerta = new AlertDialog.Builder(MainActivity.this);
+        alerta.setTitle("Salvar");
+        alerta.setIcon(android.R.drawable.ic_dialog_alert);
+        alerta.setMessage( texto );
+        alerta.setNeutralButton("Cancelar", null);
+        alerta.setPositiveButton("SIM", null);
+        alerta.setNegativeButton("NÃO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        alerta.show();
 
 
 
